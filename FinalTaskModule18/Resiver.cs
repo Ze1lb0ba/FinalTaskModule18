b84432bcec23aa39a4c9d8368796cde48ea3e5b1
stank <stankevich_dg@mail.ru>
2023-02-11 00:32:21 +0400

    with try and cath

--- FinalTaskModule18/Resiver.cs
+++ FinalTaskModule18/Resiver.cs
@@ -14,22 +14,38 @@ class Resiver
 
     public async void DownLoad()
     {
-        Console.WriteLine("Скачивание начато");
-        var videos = new YoutubeClient();
-        var streamManifest = await videos.Videos.Streams.GetManifestAsync(_data);
-        var streamInfo = streamManifest.GetMuxedStreams().GetWithHighestVideoQuality();
-        await videos.Videos.Streams.DownloadAsync(streamInfo, $"{_setting}\\video.{streamInfo.Container}");
-        Console.WriteLine("Скачивание окончено");
+        try
+        {
+            Console.WriteLine("Скачивание начато");
+            var videos = new YoutubeClient();
+            var streamManifest = await videos.Videos.Streams.GetManifestAsync(_data);
+            var streamInfo = streamManifest.GetMuxedStreams().GetWithHighestVideoQuality();
+            await videos.Videos.Streams.DownloadAsync(streamInfo, $"{_setting}\\video.{streamInfo.Container}");
+            Console.WriteLine("Скачивание окончено");
+        }
+        catch
+        {
+            Console.WriteLine("Вы указали неверную ссылку на видео");
+            EnterDataByUser.URLSet();
+        }
     }
 
     public async void Description()
     {
-        Console.WriteLine("Отправлен запрос на получение описания к видео");
-        var videos = new YoutubeClient();
-        var video = await videos.Videos.GetAsync(_data);
-        var title = video.Title;
-        Console.WriteLine();
-        Console.WriteLine("Описание видео получено");
-        Console.WriteLine(title);
+        try
+        {
+            Console.WriteLine("Отправлен запрос на получение описания к видео");
+            var videos = new YoutubeClient();
+            var video = await videos.Videos.GetAsync(_data);
+            var title = video.Title;
+            Console.WriteLine();
+            Console.WriteLine("Описание видео получено");
+            Console.WriteLine(title);
+        }
+        catch
+        {
+            Console.WriteLine("Вы указали неверную ссылку на видео");
+            EnterDataByUser.URLSet();
+        }
     }
 }
